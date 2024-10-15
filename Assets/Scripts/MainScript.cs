f334802d780d52fd3089c4e8d5226fa0fefb91c7
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-10-15 08:48:07 +0300

    Изменение скрипта
    Кнопки теперь пропадают после нажатия
    Текст активного игрока меняется

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum ThisPlayer 
+{ 
+    Zero,
+    Dagger
+}
+public class MainScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] buttons_array;
+
+    [SerializeField]
+    private Text active_Player;
+
+    [SerializeField]
+    private GameObject zero;
+
+    [SerializeField]
+    private GameObject dagger;
+
+    private byte active_element = (byte)ThisPlayer.Zero;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void IfPlayerClickButtoon(GameObject button)
+    {
+        if (active_element == (byte)ThisPlayer.Zero)
+        {
+            active_element = (byte)ThisPlayer.Dagger;
+            active_Player.text = "Очередь игрока, играющего ноликами";
+        }
+        else
+        {
+            active_element = (byte)ThisPlayer.Zero;
+            active_Player.text = "Очередь игрока, играющего крестиками";
+        }
+
+        button.SetActive(false);
+    }
+
+    public void OnClickButton_1_1()
+    {
+
+        IfPlayerClickButtoon(buttons_array[0]);
+
+        
+    }
+    public void OnClickButton_1_2()
+    {
+        IfPlayerClickButtoon(buttons_array[1]);
+    }
+    public void OnClickButton_1_3()
+    {
+        IfPlayerClickButtoon(buttons_array[2]);
+    }
+
+    public void OnClickButton_2_1()
+    {
+        IfPlayerClickButtoon(buttons_array[3]);
+    }
+    public void OnClickButton_2_2()
+    {
+        IfPlayerClickButtoon(buttons_array[4]);
+    }
+    public void OnClickButton_2_3()
+    {
+        IfPlayerClickButtoon(buttons_array[5]);
+    }
+
+    public void OnClickButton_3_1()
+    {
+        IfPlayerClickButtoon(buttons_array[6]);
+    }
+    public void OnClickButton_3_2()
+    {
+        IfPlayerClickButtoon(buttons_array[7]);
+    }
+    public void OnClickButton_3_3()
+    {
+        IfPlayerClickButtoon(buttons_array[8]);
+    }
+}
